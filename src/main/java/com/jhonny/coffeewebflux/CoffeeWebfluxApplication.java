7e29b9c986d653dabe03c3c81c76ca53fd2b7223
jhonnysierrap <jhonnysierrap@gmail.com>
2022-06-23 10:28:44 -0500

    first commit

---
+++ src/main/java/com/jhonny/coffeewebflux/CoffeeWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.jhonny.coffeewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonny/coffeewebflux/CoffeeWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonny.coffeewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
